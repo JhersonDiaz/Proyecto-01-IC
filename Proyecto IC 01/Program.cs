5d61f6ab8febb1fa9b9014875f45cb1902bfce24
Jherson Kelvin <diazbravojhersonkelvin@gmail.com>
2024-11-14 12:18:54 -0500

    Segundo cambio en git

--- Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es: Jherson Kelvin");
+
+//SEgundo commit
+Console.WriteLine("Este es mi segundo cambio en github");
+Console.ReadLine();
